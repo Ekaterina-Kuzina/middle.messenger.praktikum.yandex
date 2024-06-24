381a797241867bbb7da05753e5dd010b1086f1d0
Ekaterina Kuzina <katya.cuzina2011@yandex.ru>
2024-06-24 21:51:43 +0300

    feat: add fix

--- server.js
+++ server.js
@@ -9,10 +9,10 @@ const distPath = path.join(process.cwd(), 'dist');
 app.use(express.static(path.join(distPath)));
 
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(distPath, 'index.html'));
-    res.status(200);
+  res.sendFile(path.join(distPath, 'index.html'));
+  res.status(200);
 });
 
 app.listen(PORT, function () {
-    console.log(`Example app listening on port ${PORT}!`);
+  console.log(`Example app listening on port ${PORT}!`);
 });
